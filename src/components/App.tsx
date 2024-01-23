1ef76ff407928eef26079c006b326a633f08ae3b
Jakob Douglas <jakobdouglas1@gmail.com>
2024-01-23 16:46:15 +1030

    feat:mockup/test app running, framework complete

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,13 +8,22 @@ import RepositoriesList from './RepositoriesList';
 function App() {
 	return (
 		<Provider store={store}>
-			<div className='App pt-4 flex flex-col justify-center w-[100vw] items-center text-center'>
-				<h1 className='font-bold border p-2 rounded-lg'>
-					{' '}
-					Search for a Package
-				</h1>
-				<br />
-				<RepositoriesList />
+			<div className='max-w-[100vw] italic footer-title p-2 m-0 bg-base-300 overflow-hidden text-center'>
+				<p> search for NPM packages</p>
+			</div>
+			<div className='App pt-4 flex flex-col justify-between max-w-[100vw] h-[100vh] items-center text-center'>
+				<div className=''>
+					<h1 className='font-bold border p-2 rounded-lg'>
+						Search for a Package
+					</h1>
+					<br />
+					<RepositoriesList />
+				</div>
+				<footer className='bottom-0 left-0 max-w-[100vw] footer footer-center p-4 bg-base-300 text-base-content'>
+					<aside>
+						<p>Copyright © 2024 - All right reserved by Jakob Douglas</p>
+					</aside>
+				</footer>
 			</div>
 		</Provider>
 	);
--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -7,13 +7,14 @@ const RepositoriesList: React.FC = () => {
 	//CONTROLLED STATE FOR SEARCH TERM
 	const [term, setTerm] = useState<string>('');
 	//REF OBJECT FOR SEARCH INPUT
+	const [staticTerm, setStaticTerm] = useState<string>('');
 	const searchInput = useRef<HTMLInputElement | null>(null);
 	//getting state out of the storestate
 	// instead of annotating here weve made a hook where its annotated
 	const { loading, error, data } = useTypedSelector(
 		(state) => state.repositories
 	);
-	console.log(data);
+
 	// useEffect here just focuses the user on the input on render
 	useEffect(() => {
 		if (!searchInput.current) {
@@ -26,6 +27,8 @@ const RepositoriesList: React.FC = () => {
 		e.preventDefault();
 		//using our dispatch hook
 		searchRepositories(term);
+		setStaticTerm(term);
+		setTerm('');
 	}
 
 	return (
@@ -37,18 +40,52 @@ const RepositoriesList: React.FC = () => {
 				onSubmit={onSubmit}
 			>
 				<input
+					placeholder='Enter a Package Name'
 					name='search'
-					className='rounded-xl text-center'
+					className='rounded-xl text-center placeholder:text-gray-600'
 					ref={searchInput}
 					type='text'
 					value={term}
 					onChange={(e) => setTerm(e.target.value)}
 				/>
-				<button className=' border p-1 rounded-lg hover:bg-gray-700 hover:text-gray-900 transition ease-in-out duration-700'>
+				<button className=' border p-1 rounded-lg hover:bg-gray-700 hover:text-gray-900 transition ease-in-out duration-700 '>
 					Search
 				</button>
 			</form>
-			<h1 className='font-bold border-b mt-10'>RepositoriesList</h1>
+			{loading && (
+				<span className='loading loading-ring loading-md mt-8'></span>
+			)}
+			{error && <h3 className='text-red-500'>An error occurred: {error}</h3>}
+			{!error && !loading && data.length > 0 && (
+				<>
+					<h1 className='font-bold border-b mt-10 '>RepositoriesList</h1>
+					<div role='alert' className='alert alert-success mt-3 p-1 text-sm'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='stroke-current shrink-0 h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+							/>
+						</svg>
+						<span>
+							We've found {data.length} results for {staticTerm}
+						</span>
+					</div>
+					<div className='flex flex-col mt-6 gap-2 pb-6 max-w-[300px]'>
+						{data.map((result) => (
+							<kbd key={result} className='kbd kbd-md text-wrap'>
+								{result}
+							</kbd>
+						))}
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
